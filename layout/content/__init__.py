c27a09078111925a1ee8965e66e49636e87ee025
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-19 11:01:22 +0900

    korea only

--- layout/content/__init__.py
+++ layout/content/__init__.py
@@ -3,10 +3,14 @@ from .dashboard import dashboard
 from .momentum import momentum
 
 def content():
-    tab1, tab2, tab3 = st.tabs(["Dashboard", "Long & Short", "Long Only"])
+    tab1, tab2, tab3, tab4 = st.tabs(
+        ["Dashboard", "Long & Short", "Long Only", "Korea Only"]
+    )
     with tab1:
         dashboard()
     with tab2:
         momentum("./data/long_short.csv")
     with tab3:
         momentum("./data/long_only.csv")
+    with tab4:
+        momentum("./data/kor_only.csv")
