e277e93b37c7a991a8545eaed81ee330f73ff90f
qus0in <brillante31@naver.com>
2023-03-24 07:48:57 +0900

    path

--- 0.2/layout/content/__init__.py
+++ 0.2/layout/content/__init__.py
@@ -9,10 +9,6 @@ def content():
     with tab1:
         dashboard()
     with tab2:
-        momentum("./data/long_short.csv")
+        momentum("./0.2/data/long_short.csv")
     with tab3:
-        momentum("./data/long_only.csv")
-#   with tab4:
-#       momentum("./data/kor_only.csv")
-#   with tab5:
-#       momentum("./data/leverage.csv")
+        momentum("./0.2/data/long_only.csv")
