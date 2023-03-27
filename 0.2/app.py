39bc416528407a98b9e86c9265d8e853ae5a6e6d
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-27 10:52:22 +0900

    money

--- 0.2/app.py
+++ 0.2/app.py
@@ -1,7 +1,12 @@
 from layout.content import content
 from layout.sidebar import sidebar
+import streamlit as st
 
 def main():
+    st.set_page_config(
+        page_title="돈 복사기",
+        page_icon=":moneybag:"
+    )
     sidebar()
     content()
 
