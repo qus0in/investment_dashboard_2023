6e456fce5abf79af39d7a69651ade1cff95bf6bf
qus0in <brillante31@naver.com>
2023-03-31 21:16:27 +0900

    comment

--- 0.2/layout/content/momentum.py
+++ 0.2/layout/content/momentum.py
@@ -3,38 +3,53 @@ import streamlit as st
 import plotly.graph_objects as go
 import data.finance as finance
 
+# momentum 함수 정의
 def momentum(path: str):
+    # csv 파일에서 ticker 컬럼을 가져옴
     tickers = pd.read_csv(path, dtype=str).ticker
 
+    # 사용자가 입력한 일수를 가져옴
     days = int(st.session_state['days'])
+
+    # 모든 ETF의 종가 데이터를 가져와서 hs에 저장
     hs = pd.concat([finance.get_ohlcv(t).Close for t in tickers], axis=1)
     hs.columns = tickers
 
+    # 모멘텀 스코어 계산
     sc = []
     for i in hs.keys():
         v = hs.loc[:, i]
-        # pv = lambda x: pd.Series(
-        #     [v[vi - pd.DateOffset(months=x):].iloc[0] for vi in v.index],
-        #     index=v.index)
-        ppv = lambda x: v.rolling(20 * x).apply(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0]) / x
+        # 1, 3, 6, 12개월 모멘텀 스코어 계산
+        def ppv(x): return v.rolling(
+            20 * x).apply(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0]) / x
         r = ppv(1) + ppv(3) + ppv(6) + ppv(12)
+        # 계산된 모멘텀 스코어를 sc 리스트에 추가
         sc.append(r.tail(days))
+    # 모든 ETF의 모멘텀 스코어를 scores에 저장
     scores = pd.concat(sc, axis=1)
+
+    # 오늘의 모멘텀 스코어를 today에 저장
     today = scores.copy().iloc[[-1]].T
     today.columns = ['모멘텀 스코어']
     today['종목명'] = [finance.get_etf_name(c) for c in hs.columns]
     today['종목코드'] = hs.columns.copy()
+    # 모멘텀 스코어를 기준으로 내림차순 정렬
     today.sort_values(
         ascending=False,
         by='모멘텀 스코어',
         inplace=True
     )
+    # 모멘텀 스코어가 높은 ETF에는 👑, 그 외에는 🔎, 모멘텀 스코어가 음수인 경우에는 ✋ 표시
     today['진입'] = today['모멘텀 스코어'].apply(
         lambda x: ('👑' if x >= today.iloc[1, 0] else '🔎') if x >= 0 else '✋'
     )
+    # 종목코드를 인덱스로 설정하고 필요한 컬럼만 추출하여 today에 저장
     today = today.set_index('종목코드')
-    today = today.iloc[:,[1,2,0]]
+    today = today.iloc[:, [1, 2, 0]]
+    # streamlit의 table을 이용하여 today를 출력
     st.table(today)
+
+    # 모멘텀 스코어 트렌드를 그래프로 출력
     scores.columns = [finance.get_etf_name(c) for c in hs.columns]
     fig = go.Figure()
     for ppi in scores:
@@ -46,11 +61,12 @@ def momentum(path: str):
         title=f"1+3+6+12개월 모멘텀 스코어 트렌드 ({days}일)",
         height=320,
         margin={
-            'l':0, 'r':0, 't':30, 'b':0,
-            'pad':1
+            'l': 0, 'r': 0, 't': 30, 'b': 0,
+            'pad': 1
         },
         # yaxis_range=[-0.5,0.5],
         # colorway=pd.read_csv("./data/colors.csv", dtype=str).hex,
     )
-    
-    st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+    # streamlit의 plotly_chart를 이용하여 그래프를 출력
+    st.plotly_chart(fig, use_container_width=True,
+                    config={'displayModeBar': False})
